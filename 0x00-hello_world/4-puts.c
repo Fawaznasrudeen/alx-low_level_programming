b9e4d3a7eda8cc50a0e7eccda270365ae0500d85
Fawaznasrudeen <nasrudeenfawaz@gmail.com>
2022-12-08 10:47:31 -1200

    ALX project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - prints the phrase "programming is like building a multilingual puzzle"
+ * Return - 0 if exited properly,non-zero if otherwise
+ *
+ */
+int main(void)
+{
+	puts("programming is like building a multilingual puzzle\n");
+	return (0);
+}
