840b886a4a8f9e2902b99fde9f4456ad31868d3c
Fawaznasrudeen <nasrudeenfawaz@gmail.com>
2022-12-13 20:53:47 -1200

    message

---
+++ 0x02-functions_nested_loops/Fawaz/0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - prints the size of various types
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %ld\n", sizeof(char));
+	printf("Size of an int: %ld\n", sizeof(int));
+	printf("Size of a long int: %ld\n", sizeof(long int));
+	printf("Size of a float: %f\n", sizeof(float));
+	return (0);
+}
